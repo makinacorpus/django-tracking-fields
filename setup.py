75aec00c1df6e246d6e79a0f35010b63cd68809b
Gagaro <gagaro42@gmail.com>
2015-06-15 10:37:24 +0200

    fix: build now include migrations

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
@@ -9,8 +9,8 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-tracking-fields',
-    version='1.0',
-    packages=['tracking_fields'],
+    version='1.0.1',
+    packages=find_packages(),
     include_package_data=True,
     license='GPLv3+',
     description=(
