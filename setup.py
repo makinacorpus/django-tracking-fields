a83ea1a8a74b5cf67b2c44b2494ae84c31b12633
Gagaro <gagaro42@gmail.com>
2022-04-07 10:54:00 +0200

    back to dev

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-tracking-fields',
-    version='1.3.7',
+    version='1.3.8dev',
     packages=find_packages(),
     include_package_data=True,
     license='GPLv3+',
